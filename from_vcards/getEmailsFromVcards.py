c8fbbf959092b8073ac29da382f6173f4a21d1c1
svassili <svassiliev1@brocku.ca>
2019-01-09 09:32:58 -0400

    script structure

--- from_vcards/getEmailsFromVcards.py
+++ from_vcards/getEmailsFromVcards.py
@@ -8,8 +8,40 @@ import io
 # Installation:
 # sudo pip install beautifulsoup4 requests progress vobject
 
-dept_field = 6
 
+def get_html_page(url):
+    try:
+        vc = requests.get(url, timeout=5).content
+    except requests.exceptions.ConnectTimeout:
+        try:
+            vc = requests.get(url, timeout=10).content
+        except requests.exceptions.ConnectTimeout:
+            try:
+                vc = requests.get(url, timeout=10).content
+            except requests.exceptions.ConnectTimeout:
+                print "Connection failed!\n"
+                exit()
+    return(vc)
+
+
+def get_contact_from_vcard(url):
+    vcard = vobject.readOne(vc)
+    given = vcard.contents['n'][0].value.given
+    family = vcard.contents['n'][0].value.family
+    title = []
+    if vc.find("EMAIL") != -1:
+        email = vcard.contents['email'][0].value
+    else:
+        email = ""
+    if vc.find("TITLE") != -1:
+        for ti in vcard.contents['title']:
+            title.append(ti.value)
+    line = given + ';' + family + ';' + email + ';' + ','.join(title)
+    line = line.decode('utf-8') + '\n'
+    return(line)
+
+
+dept_field = 6
 head = u"firstname; lastname; email; title\n"
 f1 = open(sys.argv[1], "r")
 for url in f1:
@@ -24,6 +56,7 @@ for url in f1:
     tables = soup.find_all("table")
     skip = 0
     bl = 0
+    el = 0
     for each_table in tables:
         tr = each_table.find_all('tr')
         td = tr[0].find_all('td')
@@ -36,37 +69,24 @@ for url in f1:
             bar.next()
             if str(td[2]).find("phonebook") == -1:
                 continue
-            phBk = td[2].a['href'].encode("utf-8").replace("?dn", "?vcard")
-            try:
-                vc = requests.get(phBk, timeout=5).content
-            except requests.exceptions.ConnectTimeout:
-                try:
-                    vc = requests.get(phBk, timeout=10).content
-                except requests.exceptions.ConnectTimeout:
-                    try:
-                        vc = requests.get(phBk, timeout=10).content
-                    except requests.exceptions.ConnectTimeout:
-                        print "Connection failed!\n"
-                        exit()
+            url = td[2].a['href'].encode("utf-8").replace("?dn", "?vcard")
+            vc = get_html_page(url)
             if vc.find("VCARD") != -1:
-                vcard = vobject.readOne(vc)
-                given = vcard.contents['n'][0].value.given
-                family = vcard.contents['n'][0].value.family
-                additional = vcard.contents['n'][0].value.additional
-                email = ""
-                title = []
-                if vc.find("EMAIL") != -1:
-                    email = vcard.contents['email'][0].value
-                if vc.find("TITLE") != -1:
-                    for ti in vcard.contents['title']:
-                        title.append(ti.value)
-                line = given + ';' + family + ';' + \
-                    email + ';' + ','.join(title) + '\n'
-                line = line.decode('utf-8')
+                line = get_contact_from_vcard(url)
+                if line.split(';')[2] == '':
+                    el = el + 1
+                if line.split(';')[3] == '\n':
+                    line = line.strip('\n') + td[1].text.strip('\n') + "\n"
             else:
-                bl += 1
+                bl = bl + 1
                 spl = td[0].text.split(" ")
-                line = spl[0] + ";" + spl[1] + ";;\n"
+                given = spl[0]
+                spl.pop(0)
+                family = ''
+                for i in spl:
+                    family = family + i
+                line = given + ";" + family + ";;" + td[1].text.strip('\n')
+                line = line + "\n"
             f2.write(line)
         bar.finish()
     f2.close()
@@ -77,4 +97,6 @@ for url in f1:
         print ln
     if bl != 0:
         print "** Warning: " + str(bl) + " broken hyperlink(s) to vcards"
+    if el != 0:
+        print "** Warning: " + str(el) + " emails missing in vcards"
 f1.close()
